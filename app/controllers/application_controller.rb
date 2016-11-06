3b2f513469cc4da33445329daec30c1793c6bb3c
EricZhang0817 <zjh08177@gmail.com>
2016-11-05 22:38:02 -0500

    4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def index
     @name = params[:donor_name]
+  	File.new("data.csv", "w+") do |f|
+  		@name.each do |name|
+  			f << [name].join(", ") + "\n"
+  		end
+  	end
   end
 end
