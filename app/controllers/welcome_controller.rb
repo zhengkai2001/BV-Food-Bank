9a3e9dac4163371715492e66e2ff325797dbe07b
EricZhang0817 <zjh08177@gmail.com>
2016-11-06 00:41:03 -0500

    26

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
-require 'csv'
+
 
 class WelcomeController < ApplicationController
 	def index
+		require 'csv'
     	@name = params[:donor_name]
     	CSV.open("myfile.csv", "wb") do |csv|
   			csv << ["row", "of", "CSV", "data"]
