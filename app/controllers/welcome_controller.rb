6ae2337a588e91971d4d9f5d4c9cfaafa7b5c2f0
EricZhang0817 <zjh08177@gmail.com>
2016-11-06 00:24:42 -0500

    23

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,9 @@
 class WelcomeController < ApplicationController
 	def index
     	@name = params[:donor_name]
-    	respond_to do |format|
-      		format.html
-      		format.xlsx {
-    			response.headers['Content-Disposition'] = 'attachment; filename="Donor.xlsx"'
-  			}
-    	end
+    	CSV.open("myfile.csv", "w") do |csv|
+  			csv << ["row", "of", "CSV", "data"]
+  			csv << [@name, "row"]
+  		end
   	end
 end
