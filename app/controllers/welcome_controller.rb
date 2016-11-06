f3aa57d69fef36d460156c03f659c480d81157db
EricZhang0817 <zjh08177@gmail.com>
2016-11-05 23:40:34 -0500

    10

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,6 @@
 class WelcomeController < ApplicationController
 	def index
     	@name = params[:donor_name]
-    	respond_to do |format|
-      		format.html
-      		format.xlsx {
-    			response.headers['Content-Disposition'] = 'attachment; filename="Donor.xlsx"'
-  			}
-    	end
+    	
   	end
 end
