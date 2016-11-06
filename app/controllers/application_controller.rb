a26962d45aab19dedfbfa234f52f54615a0a3e1e
EricZhang0817 <zjh08177@gmail.com>
2016-11-05 22:57:24 -0500

    5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  def index
-    @name = params[:donor_name]
-  	File.new("data.csv", "w+") do |f|
-  		@name.each do |name|
-  			f << [name].join(", ") + "\n"
-  		end
-  	end
-  end
+
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,10 @@
 class WelcomeController < ApplicationController
+	def index
+    	@name = params[:donor_name]
+  		File.new("data.csv", "w+") do |f|
+  			@name.each do |name|
+  				f << [name].join(", ") + "\n"
+  			end
+  		end
+  	end
 end
