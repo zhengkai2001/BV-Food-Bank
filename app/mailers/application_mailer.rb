42cab0f38720bd4804069b4c191f53917ac0a329
EricZhang0817 <zjh08177@gmail.com>
2016-11-22 16:00:45 -0600

    es

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'app58916837@heroku.com'
   layout 'mailer'
 end
--- app/mailers/s_mailer.rb
+++ app/mailers/s_mailer.rb
@@ -1,6 +1,6 @@
 class SMailer < ApplicationMailer
 include SendGrid 
-	default from: "zjh08177@gmail.com", return_path: "zjh08177@gmail.com"
+	default from: "app58916837@heroku.com", return_path: "zjh08177@gmail.com"
 	layout 'mailer' 
 	def sendemail(email)
 		mail(to: email, subject: "Thank you for your donation")
