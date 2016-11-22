cf37f7a5756c21d07ac519de603f36194af3074c
EricZhang0817 <zjh08177@gmail.com>
2016-11-22 16:33:51 -0600

    sfs

--- app/mailers/s_mailer.rb
+++ app/mailers/s_mailer.rb
@@ -3,6 +3,6 @@ include SendGrid
 	default from: "app58916837@heroku.com", return_path: "zjh08177@gmail.com"
 	layout 'mailer' 
 	def sendemail(email)
-		mail(to: email, subject: "Thank you for your donation")
+		mail(to: email, subject: "Thank you for your donation!!!!!")
 	end
 end
